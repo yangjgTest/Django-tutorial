45ef767de481e226ad9231f681191937e43ae643
yang960701 <yang960701@gmail.com>
2022-06-22 16:59:06 +0900

    urlname

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+
+app_name = "polls"
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,7 @@
+from secrets import choice
 from django.contrib import admin
 from .models import Question
+from .models import Choice
 
 admin.site.register(Question)
+admin.site.register(Choice)
