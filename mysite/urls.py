dd432b8e079e0b4c4cfde2bae335a7a35d5da9a0
yang960701 <yang960701@gmail.com>
2022-06-23 16:15:16 +0900

    choic

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 
-app_name = "polls"
+
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
